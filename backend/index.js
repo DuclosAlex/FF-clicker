df645e01fd0765a06985340213678bcfc0558108
Duclos Alex <duclos.alex85@outlook.fr>
2022-12-19 20:26:34 +0100

    Ajout de multer pour gérer les formData

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const { personnageBaseRouter, usersRouter, monsterRouter } = require('./app/router');
 const cors = require('cors');
-
+const multer = require('multer');
 const app = express();
+
 
 app.use(express.json())
 
@@ -11,6 +12,9 @@ app.use(cors())
 
 app.use(express.urlencoded({extended : true}));
 
+const bodyParser = multer();
+
+app.use(bodyParser.none());
 
 app.use('/personnageBase', personnageBaseRouter); 
 app.use('/users', usersRouter);
--- frontend/assets/js/modules/connexion/register.js
+++ frontend/assets/js/modules/connexion/register.js
@@ -10,13 +10,28 @@ const registerModule = {
         
     },
 
-    handleAddUserForm(event) {
+    async handleAddUserForm(event) {
 
         event.preventDefault();
 
         const formElem = event.target;
 
         const formDataObject = new FormData(formElem);
+
+        try {
+
+            const response = await fetch(`http://localhost:3000/users`, {
+
+            method : 'POST',
+            body : formDataObject
+
+            });
+
+            const newUser = await response.json();
+            console.log(newUser);
+        } catch(e) {
+            console.log(e);
+        }
 
     }
 
